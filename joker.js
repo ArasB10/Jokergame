1b0011fa50401452a323ebe29023dd5f52ad2748
ArasB10 <arasbraziunas10@gmail.com>
2017-02-27 22:04:23 +0200

    Add files via upload

---
+++ joker.js
@@ -0,0 +1,94 @@
+var snake;
+var scl = 40; // Zaidimo paveiksliuku dydzio ir judejimo greicio kintamasis
+var food;
+var laughSound;
+var batmanImage;
+var currentScene = 1;
+
+// Uzkrauname garsus, paveiksliukus, sukuriame canvas ir globalius kintamuosius
+
+function setup() {
+	createCanvas(400,400);
+	snake = new Snake();
+	frameRate(10);
+	pickLocation();
+	laughSound = loadSound("sound/laugh.mp3");
+	laughSound.playMode('restart');
+	laughSound.setVolume(0.1);
+	batmanImage = loadImage("images/batman.png");
+};
+
+function draw() {
+	switch(currentScene){
+		case 1:
+			drawScene1();
+			break;
+			
+		case 2:
+			drawScene2();
+			break;
+			
+		default:
+			break;		
+	}
+};
+
+function drawScene1(){
+	background(187,187,187);
+	textSize(40);
+	fill(0);
+	text("		Joker\nsnake game", 200, 100);
+	fill(85,10,85);
+	rect(250,200,150,50);
+	fill(0);
+	text("Start",280,240);
+	if((mouseX>250 && mouseX<400)&&(mouseY>200 && mouseY<250)){
+		fill(13,108,13,100);
+		rect(250,200,150,50);
+	}
+}
+
+function drawScene2(){
+	background(187);
+	snake.update();
+	snake.show();
+
+	if(snake.eat(food)){
+		pickLocation();
+		laughSound.jump(6,2);
+	}
+	
+	image(batmanImage,food.x, food.y, scl, scl); // nupiesiame nauja arba toliau perpiesiame batman'o png
+}
+
+// Funkcija randa nauja vieta batman'o paveiksliukui atsirasti
+
+function pickLocation() {
+	var cols = floor(width/scl);
+	var rows = floor(height/scl);
+
+	food = createVector(floor(random(cols)), floor(random(rows)));
+	food.mult(scl);		
+}
+
+// Apdorojame mygtuku paspaudimus
+
+function keyPressed(){
+	if(keyCode === UP_ARROW) {
+		snake.dir(0,-1);
+	} else if (keyCode === DOWN_ARROW) {
+		snake.dir(0,1);
+ 	} else if (keyCode === RIGHT_ARROW) {
+		snake.dir(1,0);
+	} else if (keyCode === LEFT_ARROW) {
+		snake.dir(-1,0);
+	}
+}
+
+function mouseClicked(){
+	if(currentScene===1){
+		if((mouseX>250 && mouseX<400)&&(mouseY>200 && mouseY<250)){
+			currentScene=2;
+		}
+	}
+}
---
+++ snake.js
@@ -0,0 +1,50 @@
+function Snake(){
+	this.x = 0;
+	this.y = 0;
+	this.xspeed = 0;
+	this.yspeed = 0;
+	this.total = 0;
+	this.tail = [];
+	this.joker = loadImage("images/joker.png");
+	this.badguy = loadImage("images/bad.png");
+
+	this.update = function() {
+		for(var i = 0; i< this.tail.length-1; i++){
+			this.tail[i] = this.tail[i+1];
+		}
+
+		this.tail[this.total-1] = createVector(this.x, this.y);
+
+		this.x = this.x + this.xspeed*scl;
+		this.y = this.y + this.yspeed*scl;
+
+		this.x = constrain(this.x, 0, width-scl);
+		this.y = constrain(this.y, 0, height-scl);
+	}
+
+	this.show = function() {
+		fill(255);
+		for(var i = 0; i< this.total; i++){
+			image(this.badguy,this.tail[i].x, this.tail[i].y, scl, scl);
+			//rect(this.tail[i].x, this.tail[i].y, scl, scl);
+		}
+
+		image(this.joker,this.x, this.y, scl, scl);
+		//rect(this.x, this.y, scl,scl);
+	}
+
+	this.dir = function(x,y){
+		this.xspeed = x;
+		this.yspeed = y;
+	}
+
+	this.eat = function(pos){
+		var d = dist(this.x, this.y, pos.x, pos.y);
+		if(d < 1){
+			this.total++;
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
